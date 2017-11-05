f0038d7b71d36e37d9939bf83eccca5be5712fed
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2017-11-05 11:26:56 +0000

    Add initial plugin page

---
+++ autoschedule.php
@@ -0,0 +1,11 @@
+<?php
+    /*
+    Plugin Name: Auto Scheduling
+    Plugin URI: https://www.hernantas.com/autoschedule
+    Description: a plugin to automatically set your post schedule
+    Version: 1.0
+    Author: Zulfikar Raditya Hernanta
+    Author URI: https://www.hernantas.com
+    License: MIT
+    */
+?>
